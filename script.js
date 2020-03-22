05ffa2c9a78268fb26d5d27e2b816e27388cedc6
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-03-22 17:47:03 -0400

    updated current weather js format and included date; reformatted html

--- script.js
+++ script.js
@@ -18,20 +18,23 @@ $(".search").on("click", function () {
         .then(function (response) {
             console.log(response);
             console.log(response.name);
-            var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            var cityName = response.name // open weather API's city name
+            var iconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; // current weather icon
             var icon = $("<img>").attr("src", iconUrl);
-            var temp = $("<p>").text("Teperature: " + response.main.temp);
+            var fahrTemp = ((response.main.temp - 271.15) * 1.8 + 32).toFixed(1); // converts open weather API 
+            var temp = $("<p>").html("Teperature: " + fahrTemp + " &deg;F");
             var humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
-            var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
+            var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
+            var date = new Date(response.dt*1000).toLocaleDateString("en-US"); // convert dt to date
 
-            $("#city").append($("<p>").text(searchCity));
+            $("#city").append($("<h3>").text(cityName + " (" + date + ")"));
             $("#city").append(icon);
             $("#tempurature").append(temp);
             $("#humid").append(humidity);
             $("#wind").append(windSpeed);
 
-            var lat = response.coord.lat;
-            var lon = response.coord.lon;
+            var lat = response.coord.lat; // grab lattitude from current weather API for UV Index API
+            var lon = response.coord.lon; // grab longitude from current weather API for UV Index API
             var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
 
             $.ajax({
@@ -40,26 +43,36 @@ $(".search").on("click", function () {
             })
                 .then(function (response) {
                     console.log(response);
-                    var UV = $("<p>").text("UV Index: " + response.value);
+                    var UV = response.value;
+                    var UVel = $("<p>").text("UV Index: ");
+                    console.log(UV);
 
-                    $("#uv-index").append(UV);
+                    $("#uv-index").append(UVel);
 
-                    // if (UV ) //change background color depend on UV condition
+                    //change background color of UV index depending on UV condition
+                    // if (UV < 2) {
+                    //     $("#uv-index").append(UV).attr("class", "badge badge-primary");
+                    // } else if (UV >= 2 && UV < 5) {
+                    //     $("#uv-index").append(UV).attr("class", "badge badge-warning");
+                    // } else {
+                    //     $("#uv-index").append(UV).attr("class", "bg-danger text-white");
+                    // }
+                    // })
+                });
+
+            $("#forecast").text("5-Day Forecast:")
+
+            var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + APIKey;
+
+            $.ajax({
+                url: fiveDayURL,
+                method: "GET"
+
+            })
+                .then(function (response) {
+                    console.log(response);
+
                 })
+
         });
-
-    var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + APIKey;
-
-    $.ajax({
-        url: fiveDayURL,
-        method: "GET"
-
-    })
-        .then(function (response) {
-            console.log(response);
-
-        })
-
-});
-
-
+    });
